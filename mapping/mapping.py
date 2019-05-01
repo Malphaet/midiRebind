67d80e5980116df21b04a3a185725b630b7f8559
Maximilien Rigaut <max.rigaut@gmail.com>
2019-05-01 16:30:36 +0200

    Added the test module and refined the code

--- mapping/mapping.py
+++ mapping/mapping.py
@@ -4,17 +4,18 @@ class BasicMessageParse(object):
 
     def __init__(self):
         '''Basic init'''
-        self.mapping={"value":None,"type":None} # A barebone initialisation, unusable, it will need to be overridden to work
-        self.value=None
-        self.
+        self._mapping={"value":None,"type":None} # A barebone initialisation, unusable, it will need to be overridden to work
 
     def parseMidi(self,message):
-        """Take a midi SyxEx message and return a BasicMapping"""
-        pass
+        """Take a midi SyxEx message and return a BasicMapping (there can be many children of BasicMapping to take more properly care of the cases)"""
+        return BasicMapping(self._mapping)
 
 class BasicMapping(object):
     """A basic reading of the message, propreties are initialised depending on the way the SyEx message is formed,
     this objet will need the configuration to read and understand the message
     """
-    def __init__(self):
-        pass
+    def __init__(self,dictmapping):
+        'Barebone init'
+        self._mapping=dictmapping
+
+    def 
