35c475b5db2f92b6f718a81dba5026cbea2ee75e
dandaedre <max.rigaut@gmail.com>
2021-09-24 17:34:06 +0200

    Working First Draft

--- mapping/keystrokes.py
+++ mapping/keystrokes.py
@@ -8,22 +8,24 @@ import re,configparser
 from mapping.midi import MessageParse
 from mapping.midi import Actions
 
+from threading import Timer
+
 __TESTING=True
 
+
+class pyautonope(object):
+    def __init__(self):
+        pass
+
+    def __call__(self,*args,**kwargs):
+        return self
+
+    def __repr__(self,*args,**kwargs):
+        return ''
+
+    def __getattribute__(self,*args,**kwargs):
+        return self
 if __TESTING:
-    class pyautonope(object):
-        def __init__(self):
-            pass
-
-        def __call__(self,*args,**kwargs):
-            return self
-
-        def __repr__(self,*args,**kwargs):
-            return ''
-
-        def __getattribute__(self,*args,**kwargs):
-            return self
-
     pyautogui=pyautonope()
 else:
     import pyautogui
@@ -31,10 +33,18 @@ else:
 INPUTS=  [0,0,1,0,0,0,1,0]
 MEMORIES=[1,1,1,0,0,0,0,0]
 
+_NOTIMER=pyautonope()
 
 _ACTIVE=0b1
 _SELECTED=0b10
 _LIVE=0b100
+
+#Modes
+_RESET=lambda a,b: a
+_OR=lambda a,b: a|b
+_XOR=lambda a,b: a^b
+
+_TIMERTAKE=3.0
 
 _BASEVALUES=[
     INPUTS[:],
@@ -47,25 +57,23 @@ class demvars():
         self.output=None
         self.BASE_NOTE_INPUT=56
         self.BASE_NOTE_MASTERM=48
+        self.BASE_NOTE_ARM_TAKE=89
 
         self.listchange=[]
-        self.lastpress=None #Does only account for the 8x8
+        self.lastpress=(None,None) #Does only account for the 8x8
         self.selected_pulse=(None,None)
+        self.live_pulse=(None,None)
 
-        self.live_input=None
-        self.selected_input=0
-        #
-        self.live_memory=None
-        self.selected_memory=None
-
+        self.timertake=_NOTIMER
         self.INPUTS=[i*_ACTIVE for i in INPUTS]
         self.MEMORIES=[i*_ACTIVE for i in MEMORIES]
 
         self.VALUES=[self.INPUTS,self.MEMORIES,[[0]*8]*6]
 
+        self.takearmed=False
 
         self.COLORS={"black":0,"green":1,"blinking_green":2,"red":3,"blinking_red":4,"yellow":5,"blinking_yellow":6}
-        self.COLORPLUGS={0:self.COLORS["yellow"],1:self.COLORS["green"],2:self.COLORS["blinking_green"],3:self.COLORS["red"]}
+        #self.COLORPLUGS={0:self.COLORS["yellow"],1:self.COLORS["green"],2:self.COLORS["blinking_green"],3:self.COLORS["red"]}
         self.PLUGCOLORS={
             0:                          self.COLORS["yellow"],
             _ACTIVE:                    self.COLORS["green"],
@@ -78,35 +86,27 @@ class demvars():
         }
 
     # Should do it in a similar way, cant find it
-    def addChange(self,i,j,value,reset=False):
+    def addChange(self,i,j,value,reset=True): #mode=_OR
         "Do a value change, store the change for update"
         if i==None or j==None:
             return
-        # self.listold+=[[i,j,self.VALUES[j][i]]]
-        # if value==None:
-        # if reset:
-        #     value=_BASEVALUES[j][i]
-        # else:
-        #     value=self.VALUES[]
-        self.listchange+=[[i,j,_BASEVALUES[j][i],reset]]
-        # print(self.listchange)
+        self.listchange+=[[i,j,value,reset]]
 
 
-    def appyChanges(self):
+    def applyChanges(self):
         "Apply all changes"
         allch=set()
         for i,j,v,r in self.listchange:
             allch.add((i,j))
             if r:
                 self.VALUES[j][i]=_BASEVALUES[j][i]|v
-                print(_BASEVALUES[j][i]|v,_BASEVALUES[j][i])
+                #print("({}:{}) - Action:{} Base:{} Effect:{} Color:{}".format(i,j,v,_BASEVALUES[j][i],_BASEVALUES[j][i]|v,self.PLUGCOLORS[_BASEVALUES[j][i]|v]))
             else:
                 self.VALUES[j][i]|=v
         self.listchange=[]
 
         for i,j in allch: # I feel like I'm doing this twice
-            # print(i,j,self.COLORPLUGS[self.VALUES[j][i]])
-            self.light(i,j,self.COLORPLUGS[self.VALUES[j][i]])
+            self.light(i,j,self.PLUGCOLORS[self.VALUES[j][i]])
 
         # self.
     def findit(self,col,row):
@@ -138,8 +138,9 @@ def _onload(self):
     "Send a reset colors"
     import time,sys
     vars.output=self.outputs[vars.interface_nb]
-    for i in range(64):
-        vars.lightnote(i,0)
+
+    #Cleaning the arm
+    vars.lightnote(vars.BASE_NOTE_ARM_TAKE,2)
 
     vars.updateinput()
     vars.updatemasterm()
@@ -147,32 +148,30 @@ def _onload(self):
 
 def switchinput(trigger,val,note,*params): #COULD DO WITH A CLASS AND .change(i,j,val)
     "Switch to the desired layer i1"
+
+    # These two lines could be better done
     i=note-vars.BASE_NOTE_INPUT
-    toup=[vars.selected_input,i]
-    vars.lastpress=(i,1)
+    vars.lastpress=(i,0)
 
-    vars.INPUTS[vars.selected_input]=INPUTS[vars.selected_input]
-    vars.selected_input=i
-    vars.INPUTS[vars.selected_input]|=_SELECTED
-    if vars.live_input:
-        vars.INPUTS[vars.live_input]|=_LIVE
+    vars.addChange(*vars.selected_pulse,0,reset=True) # Reset this to base
+    vars.selected_pulse=(i,0)
 
-    vars.updateinput(toup)
+    vars.addChange(*vars.lastpress,_SELECTED,reset=True) # Add a selection
+    vars.addChange(*vars.live_pulse,_LIVE,reset=False)
 
+    vars.applyChanges()
     pyautogui.hotkey("i",str(i+1))
 
 def takeall(trigger,val,note,*params):
     "Take all TA"
-    if vars.live_input!=None:
-        vars.INPUTS[vars.live_input]=INPUTS[vars.live_input]
-        toup=[vars.live_input,vars.selected_input]
-    else:
-        toup=[vars.selected_input]
 
-    vars.live_input=vars.selected_input
-    vars.INPUTS[vars.selected_input]|=_SELECTED
-    vars.INPUTS[vars.live_input]|=_LIVE
-    vars.updateinput(toup)
+    if not vars.takearmed:
+        return
+    # unarmtake()
+    vars.addChange(*vars.live_pulse,0)
+    vars.live_pulse=vars.selected_pulse
+    vars.addChange(*vars.selected_pulse,_SELECTED|_LIVE)
+    vars.applyChanges()
 
     pyautogui.hotkey("t","a")
 
@@ -185,10 +184,23 @@ def switchmastermemory(trigger,val,note,*params):
     vars.selected_pulse=(i,1)
     vars.addChange(*vars.lastpress,_SELECTED,reset=True) # Add a selection
 
-    # vars.selected_memory=i
-    # vars.addChange()
-    vars.appyChanges()
+    vars.applyChanges()
     pyautogui.hotkey("m",str(i))
+
+def unarmtake():
+    "Unarm the take button"
+    # vars.timertake.cancel()
+    vars.takearmed=False
+    vars.timertake=_NOTIMER
+    vars.lightnote(vars.BASE_NOTE_ARM_TAKE,2)
+
+def armtake(trigger,val,note,*params):
+    "Arm the take button, unarm after a short while"
+    vars.timertake.cancel()
+    vars.timertake=Timer(_TIMERTAKE, unarmtake)
+    vars.takearmed=True
+    vars.lightnote(vars.BASE_NOTE_ARM_TAKE,1) # First and only blinking value
+    vars.timertake.start()
 
 # MidiInterface(config)
 class MidiInterface(BasicMidiInterface):
@@ -200,3 +212,4 @@ class MidiInterface(BasicMidiInterface):
         self.functionlist["switchinput"]=switchinput
         self.functionlist["mastermemory"]=switchmastermemory
         self.functionlist["takeall"]=takeall
+        self.functionlist["armtake"]=armtake
