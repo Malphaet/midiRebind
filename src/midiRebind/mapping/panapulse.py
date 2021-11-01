9cd1496d2d29b353a25143fa3b8e4d1aeb120a4a
Maximilien Rigaut <max.rigaut@gmail.com>
2021-11-01 06:27:18 +0100

    Tweaking the pulseServer

--- src/midiRebind/mapping/panapulse.py
+++ src/midiRebind/mapping/panapulse.py
@@ -20,7 +20,7 @@ def printl(label=""):
 
 eprint=printl("[mPH:ERROR]")
 dprint,ddprint,iprint,wprint=nopeF,nopeF,nopeF,nopeF
-bindings.dprint, bindings.ddprint, bindings.iprint= nopeF, nopeF, nopeF
+# bindings.dprint, bindings.ddprint, bindings.iprint= nopeF, nopeF, nopeF
 
 if _VERBOSE>=1:
     wprint=printl("[mPH:WARNING]")
@@ -90,6 +90,7 @@ def pagepress(trigger,val,note,*params):
         eprint(e)
     except SystemExit as e:
         eprint("The Pulse requested a SystemExit (likely due to a connection interrupted), please check your settings")
+        sys.exit()
 
 @doublepress
 def quitpress(trigger,val,note,*params):
